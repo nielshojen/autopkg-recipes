2d7df081c9fe4efe2a9e9218ddab26a8e96b6f8a
Niels Højen <niels@hojen.net>
2017-09-22 09:41:02 +0200

    Update MASReceiptWipe.py

--- SharedProcessors/MASReceiptWipe.py
+++ SharedProcessors/MASReceiptWipe.py
@@ -20,12 +20,8 @@ class MASReceiptWipe(Processor):
 
 	def main(self):
 		filename = self.env.get('filename')
-
 		cmd = '/bin/echo 0 > ' + filename
-
-		retcode = subprocess.call([cmd])
-		
+		retcode = subprocess.call([cmd], shell = True)
 		if retcode:
 			raise ProcessorError('Error wiping MAS recipt for %s' % (filename))
-
 		return
