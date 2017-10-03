5b83321f6e98501479057578791f418d22cf819b
Niels Højen <niels@hojen.net>
2017-10-03 16:55:30 +0200

    Update VersionExtractor.py

--- SharedProcessors/VersionExtractor.py
+++ SharedProcessors/VersionExtractor.py
@@ -37,8 +37,7 @@ class VersionExtractor(Processor):
         split_on_out = self.env.get("split_on_out", " ")
         index = self.env.get("index", 1)
         self.env["version"] = self.env["version"].split(split_on_in)[index]
-        index = self.env.get("index", 0)
-        self.env["version"] = self.env["version"].split(split_on_out)[index]
+
         self.output("Version: %s" % self.env["version"])
 
 
