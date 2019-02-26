7f36ce7460126821f53cc864d7cc17a0d8a50480
Niels Højen <niels@hojen.net>
2019-02-26 10:54:27 +0100

    Update IntellijURLProvider.py

--- SharedProcessors/IntellijURLProvider.py
+++ SharedProcessors/IntellijURLProvider.py
@@ -1,82 +1,144 @@
 #!/usr/bin/env python
-"""Intellij URL Provider."""
-# Copyright (c) 2015-present, Facebook, Inc.
-# All rights reserved.
 #
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree. An additional grant
-# of patent rights can be found in the PATENTS file in the same directory.
+# Copyright 2015
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+import re
+import urllib
 import urllib2
-import xml.etree.cElementTree as ET
+import json
 
 from autopkglib import Processor, ProcessorError
 
+__all__ = ["JetbrainsURLProvider"]
 
-__all__ = ["IntellijURLProvider"]
+# New JetBrains page circa 2015 has an ajax endpoint which returns information about the
+# latest releases of each product. example JSON response:
+# { "PS":[
+#   {"date":"2016-01-08","type":"release","downloads":{
+#      "windows":{"link":"https://download.jetbrains.com/webide/PhpStorm-10.0.3.exe","size":166275856,"checksumLink":"https://download.jetbrains.com/webide/PhpStorm-10.0.3.exe.sha256"},
+#      "mac":{"link":"https://download.jetbrains.com/webide/PhpStorm-10.0.3-custom-jdk-bundled.dmg","size":195391609,"checksumLink":"https://download.jetbrains.com/webide/PhpStorm-10.0.3-custom-jdk-bundled.dmg.sha256"},
+#      "linux":{"link":"https://download.jetbrains.com/webide/PhpStorm-10.0.3.tar.gz","size":212948566,"checksumLink":"https://download.jetbrains.com/webide/PhpStorm-10.0.3.tar.gz.sha256"}
+#   },"version":"10.0.3","majorVersion":"10.0","build":"143.1770"}]}
+#
 
-intellij_version_url = 'https://www.jetbrains.com/updates/updates.xml'
+PRODUCTS = ("IntelliJ IDEA", "RubyMine", "PyCharm", "PhpStorm", "WebStorm", "AppCode")
+
+PRODUCT_CODES = {
+    "AppCode": "AC",
+    "CLion": "CL",
+    "dotCover": "DC",
+    "dotCoverCommandLineTools": "DCCLT",
+    "dotMemory": "DM",
+    "dotMemoryUnit": "DMU",
+    "dotPeek": "DPK",
+    "dotTrace": "DP",
+    "dotTraceCommandLineTools": "DPCLT",
+    "dotTraceProfilingSDK": "DPPS",
+    "hub": "HB",
+    "IntelliJ IDEA Ultimate": "IIU",
+    "IntelliJ IDEA Standard": "II",
+    "mps": "MPS",
+    "mpsIntelliJIDEAPlugin": "MPSIIP",
+    "PyCharm": "PC",
+    "PyCharm Professional": "PCP",
+    "PyCharm Community": "PCC",
+    "PyCharm EDU": "PCE",
+    "PhpStorm": "PS",
+    "RubyMine": "RM",
+    "ReSharper": "RS",
+    "ReSharper Command Line Tools": "RSCLT",
+    "ReSharper CPP": "RC",
+    "ReSharper Ultimate": "RSU",
+    "TeamCity": "TC",
+    "WebStorm": "WS",
+    "YouTrack": "YT",
+    "YouTrack Standalone": "YTD",
+    "YouTrack Workflow Editor": "YTWE",
+    "UpSource": "US",
+    "0xDBE": "DBE",
+    "DataGrip": "DG"
+}
+
+RELEASE_XHR_ENDPOINT = "https://data.services.jetbrains.com/products/releases?code={0}&latest=true&type=release&_={1}"
 
 
-class IntellijURLProvider(Processor):
-  """Provide URL for latest Intellij IDEA build."""
+DEFAULT_PRODUCT = "IntelliJ IDEA"
+DEFAULT_CODE = PRODUCT_CODES["IntelliJ IDEA Ultimate"]  # Only used for IDEA IU=Ultimate, IC=Community
+DEFAULT_PLATFORM = "mac"
 
-  description = "Provides URL and version for the latest release of Intellij."
-  input_variables = {
-    "base_url": {
-      "required": False,
-      "description": ('Default is '
-                      'https://www.jetbrains.com/updates/updates.xml'),
-    },
-    "edition": {
-      "required": False,
-      "description": ('Either "C" for "Community" or "U" for "Ultimate" '
-                      'edition. Defaults to "C".')
+
+class JetbrainsURLProvider(Processor):
+    description = "Provides URL to the latest JetBrains IDE release"
+    input_variables = {
+        "product_code": {
+            "required": True,
+            "description": "The product code. AppCode=AC, CLion=CL, IDEA Ultimate=IIU, PyCharm Pro=PCP, PhpStorm=PS, RubyMine=RM, WebStorm=WS"
+        },
+        "platform": {
+            "required": False,
+            "description": "[optional] The operating system platform, one of 'mac', 'windows', 'linux'"
+        }
     }
-  }
-  output_variables = {
-    "url": {
-      "description": "URL to the latest release of Intellij",
+    output_variables = {
+        "url": {
+            "description": "URL to the latest JetBrains IDE release",
+        },
+        "version": {
+            "description": "Product version",
+        },
+        "majorVersion": {
+            "description": "Product major version, usually first two numbers",
+        },
+        "build": {
+            "description": "Product build number",
+        }
     }
-  }
 
-  __doc__ = description
+    __doc__ = description
 
-  def get_intellij_version(self, intellij_version_url):
-    """Retrieve version number from XML."""
-    # Read XML
-    try:
-        req = urllib2.Request(intellij_version_url)
-        f = urllib2.urlopen(req)
-        html = f.read()
-        f.close()
-    except BaseException as e:
-        raise ProcessorError(
-          "Can't download %s: %s" % (
-            intellij_version_url, e)
-        )
-    # Search for download link.
-    root = ET.fromstring(html)
-    # root[0][-1] is always the last IDEA release
-    build = root[0][-1].find('build')
-    version = build.attrib['version']
-    # Return pkg url.
-    return str(version)
+    def xhr_release_info(self, product_code):
+        """Request release information from JetBrains XHR Endpoint"""
+        url = RELEASE_XHR_ENDPOINT.format(product_code, "123123123")
+        request = urllib2.Request(url)
 
-  def main(self):
-    """Main function."""
-    # Determine values.
-    version_url = self.env.get('version_url', intellij_version_url)
-    version = self.get_intellij_version(version_url)
-    download_url = (
-      "https://download.jetbrains.com/idea/"
-      "ideaI%s-%s.dmg" % (self.env.get('edition', 'C'), version)
-    )
+        try:
+            handle = urllib2.urlopen(request)
+            response = handle.read()
+            handle.close()
+        except BaseException as e:
+            raise ProcessorError("Cannot retrieve product information from JetBrains")
 
-    self.env["url"] = download_url
-    self.output("URL: %s" % self.env["url"])
+        product_info = json.loads(response)
+        return product_info[product_code][0]
 
-if __name__ == '__main__':
-    processor = IntellijURLProvider()
+
+    def main(self):
+        product_code = self.env.get("product_code", DEFAULT_CODE)
+        platform = self.env.get("platform", DEFAULT_PLATFORM)
+
+        product_info = self.xhr_release_info(product_code)
+        download_info = product_info["downloads"][platform]
+
+        self.env["url"] = download_info["link"]
+        self.env["version"] = product_info["version"]
+        self.env["majorVersion"] = product_info["majorVersion"]
+        self.env["build"] = product_info["build"]
+
+        self.output("Found URL %s" % self.env["url"])
+
+
+if __name__ == "__main__":
+    processor = JetbrainsURLProvider()
     processor.execute_shell()
